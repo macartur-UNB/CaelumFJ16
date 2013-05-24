b74619155a2f4311da37d9f9cae4574fecc5e3f8
macartur <macartur.sc@gmail.com>
2013-05-23 21:17:33 -0300

    Correçoes simples semNegocioGeraCandleComZero

--- Caelum/test/modelteste/semNegociosGeraCandleComZero.java
+++ Caelum/test/modelteste/semNegociosGeraCandleComZero.java
@@ -3,54 +3,61 @@ package modelteste;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
-import model.Candlestick;
-import model.Negocio;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import model.Candlestick;
+import model.Negocio;
+import model.CandlestickFactory;
 
 public class semNegociosGeraCandleComZero {
+    
     @Test
     public void TestaCandleComAberturaZero(){
         Calendar hoje = Calendar.getInstance();
         List<Negocio> negocios = Arrays.asList();
-        model.CandlestickFactory fabrica = new model.CandlestickFactory();
+        CandlestickFactory fabrica = new CandlestickFactory();
         Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
- 
+
         assertEquals(0, candle.getAbertura(),00001);
     }
+    
     @Test    
     public void TestaCandleComFechamentoZero(){
-     Calendar hoje = Calendar.getInstance();
-     List<Negocio> negocios = Arrays.asList();
-     model.CandlestickFactory fabrica = new model.CandlestickFactory();
-     Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
+        Calendar hoje = Calendar.getInstance();
+        List<Negocio> negocios = Arrays.asList();
+        CandlestickFactory fabrica = new CandlestickFactory();
+        Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
 
-     assertEquals(0, candle.getFechamento(),00001);
+        assertEquals(0, candle.getFechamento(),00001);
     }
+    
     @Test
     public void testaCandleComMinimoZero(){
         Calendar hoje = Calendar.getInstance();
         List<Negocio> negocios = Arrays.asList();
-        model.CandlestickFactory fabrica = new model.CandlestickFactory();
+        CandlestickFactory fabrica = new CandlestickFactory();
         Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
  
         assertEquals(0, candle.getMinimo(),00001);
     }
+    
     @Test
     public void testaCandleComMaximoZero(){
         Calendar hoje = Calendar.getInstance();
         List<Negocio> negocios = Arrays.asList();
-        model.CandlestickFactory fabrica = new model.CandlestickFactory();
+        CandlestickFactory fabrica = new CandlestickFactory();
         Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
  
         assertEquals(0, candle.getMaximo(),00001);
     }
+    
     @Test  
     public void testaCandleComVolumeZero(){
         Calendar hoje = Calendar.getInstance();
+    
         List<Negocio> negocios = Arrays.asList();
-        model.CandlestickFactory fabrica = new model.CandlestickFactory();
+        
+        CandlestickFactory fabrica = new CandlestickFactory();
         Candlestick candle = fabrica.constroiCandleParaData(hoje, negocios);
  
         assertEquals(0, candle.getVolume(),00001);
